2ccfffb20d655cd2972759837d3901b29769587e
Matias <maty.infante@gmail.com>
2022-05-04 11:29:56 -0300

    Final ej1

---
+++ js/ej1.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $.getJSON("../../../database/lista_ropa.json", function (data) {
+        let tag
+        $.each(data, function (key, value) {
+            tag += `<option id="${key}" class="material" value=${value.material}> ${value.material} </option>`
+        })
+        $('#material').append(tag)
+    })
+
+})
+
+function cargarColores(material) {
+    $.getJSON("../../../database/lista_ropa.json", function (data) {
+        let tag
+        colores = data[material].colores
+        $.each(colores, function (key, value) {
+            tag += `<option id="${value}" class="color" value=${value}> ${value} </option>`
+        })
+        $('#color').append(tag)
+        $('#precio').empty()
+        $('#precio').append(`${data[material].precio}`)
+    })
+
+    /*$.ajax({
+        url: "../../../database/lista_ropa.json",
+        type: 'GET',
+        success: function otrometodo(response) {
+            let tag
+            colores = data[material].colores
+            $.each(colores, function (key, value) {
+                tag += `<option id="${value}" class="color" value=${value}> ${value} </option>`
+            })
+            $('#color').append(tag)
+            $('#precio').empty()
+            $('#precio').append(`<p>${data[material].precio}</p>`)
+        }
+    })*/
+}
+
+$('#material').on('change', function (e) {
+    var materialSelected = $('select option').filter(':selected').attr("id");
+    $('#color').empty()
+    cargarColores(materialSelected)
+})
